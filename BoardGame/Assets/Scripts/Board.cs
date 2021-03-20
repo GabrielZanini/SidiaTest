67355debb59daba71716d239f61f30fcc2c67c21
Gabriel_Zanini <gabriel.roszan@gmail.com>
2021-03-20 16:29:08 -0300

    Create Square Board
    Main Scene can create a board using square Tiles.

--- BoardGame/Assets/Scripts/Board.cs
+++ BoardGame/Assets/Scripts/Board.cs
@@ -1,18 +1,125 @@
-﻿using System.Collections;
+﻿using NaughtyAttributes;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Board : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject tilePrefab;
+
+    [SerializeField]
+    Sprite squareTile;
+
+    [SerializeField] [Range(4, 16)]
+    int size = 16;
+
+    [ReorderableList] [SerializeField]
+    List<Color> tileColors;
+
+    [SerializeField] [ReadOnly]
+    List<Tile> board;
+
+    int totalTiles = 0;
+
+    int nextColorId = -1;
+
+    private void Reset()
     {
-        
+        if (tileColors.Count == 0)
+        {
+            tileColors.Add(Color.white);
+        }
     }
 
-    // Update is called once per frame
+    private void OnValidate()
+    {
+        Reset();
+    }
+
+    void Start()
+    {
+        CreateBoard();
+    }
+
     void Update()
     {
-        
+
     }
+
+    [Button]
+    void CreateBoard()
+    {
+        ClearBoardTiles();
+        CreateSquareBoard();
+    }
+
+    void CreateSquareBoard()
+    {
+        //Debug.Log("CreateBoard");
+        totalTiles = size * size;
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                GameObject obj = Instantiate(tilePrefab, new Vector3(j, 0, i), Quaternion.Euler(90, 0, 0), transform);
+                Tile tile = obj.GetComponent<Tile>();
+
+                tile.spriteRenderer.sprite = squareTile;
+                tile.spriteRenderer.color = GetNextColor();
+
+                tile.Neighbors = GetNeighbors();
+
+                board.Add(tile);
+            }
+            GetNextColor();
+        }
+    }
+
+    [Button]
+    void ClearBoardTiles()
+    {
+        nextColorId = -1;
+
+        while (board.Count > 0)
+        {
+            if (Application.isPlaying)
+            {
+                Destroy(board[0].gameObject);
+            }
+            else
+            {
+                DestroyImmediate(board[0].gameObject);
+            }
+            
+            board.RemoveAt(0);
+        }
+    }
+
+
+
+    Color GetNextColor()
+    {
+        //Debug.Log("nextColorId: " + nextColorId);
+        nextColorId += 1;
+
+        if (nextColorId >= tileColors.Count)
+        {
+            nextColorId = 0;
+        }
+
+        return tileColors[nextColorId];
+    }
+
+
+    List<Tile> GetNeighbors()
+    {
+        Debug.Log("-------------- Visinhos ------------");
+        return null;
+    }
+
 }
+
+
+
---
+++ BoardGame/Assets/Scripts/Tile.cs
@@ -0,0 +1,31 @@
+﻿using NaughtyAttributes;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [ReadOnly]
+    public List<Tile> Neighbors;
+    [ReadOnly]
+    public SpriteRenderer spriteRenderer;
+
+
+    private void Reset()
+    {
+        if (spriteRenderer == null)
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+    }
+
+    private void OnValidate()
+    {
+        Reset();
+    }
+
+    void Update()
+    {
+        
+    }
+}
